fcdde8d2f6b0298f48b4624bef56e999a5da1792
sadik-fattah <sadikfattah5@gmail.com>
2024-10-07 12:58:45 +0100

    fix bugs

--- app/src/main/java/com/guercifzone/hity2ube/MainActivity.java
+++ app/src/main/java/com/guercifzone/hity2ube/MainActivity.java
@@ -1,7 +1,14 @@
 package com.guercifzone.hity2ube;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +23,10 @@ import com.guercifzone.hity2ube.Lib.VideoLink;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
+EditText editText;
+Button btn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,25 +37,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-   try {
-       //VideoDownloader.getInstance().getResults(this, "https://www.youtube.com/shorts/ZTS3YLjI0XE", new OnVideoFoundListener() {
-       VideoDownloader.getInstance().getResults(this, "https://www.youtube.com/watch?v=vyqQRczjMoQ", new OnVideoFoundListener() {
-           @Override
-           public void onVideo(ArrayList<VideoLink> videos) {
-               for (int i = 0; i < videos.size(); i++) {
-                   Log.e("size", videos.get(i).isAudioAvailable() + " " + videos.get(i).getQuality() + "adf  " + videos.get(i).getUrl());
+        textView = (TextView) findViewById(R.id.title);
+        editText = (EditText) findViewById(R.id.link);
+        btn = (Button) findViewById(R.id.dwnd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadurl();
+            }
+        });
+if(editText.getText().toString().isEmpty()){
+    Toast.makeText(this, "enter youtube link", Toast.LENGTH_SHORT).show();
+    btn.setVisibility(View.GONE);
+}else {
+    btn.setVisibility(View.VISIBLE);
+    Toast.makeText(this, "plaese wait", Toast.LENGTH_LONG).show();
+}
+    }
 
-               }
-           }
+    private void loadurl() {
+        try {
+            //https://www.youtube.com/shorts/ZTS3YLjI0XE"
+            VideoDownloader.getInstance().getResults(this, editText.getText().toString(), new OnVideoFoundListener() {
+                @Override
+                public void onVideo(ArrayList<VideoLink> videos) {
+                    for (int i = 0; i < videos.size(); i++) {
+                        Log.e("size:", videos.get(i).isAudioAvailable() + " server url --> " + videos.get(i).getQuality() + " quality : " + videos.get(i).getUrl());
+                        textView.setText(videos.get(i).getUrl());
+                        textView.setTextColor(Color.BLUE);
 
-           @Override
-           public void onError(String error) {
-               Log.e("size", error);
-           }
-       });
+                    }
+                }
 
-   }catch (Exception e){
-       e.printStackTrace();
-   }
+                @Override
+                public void onError(String error) {
+                    Log.e("size", error);
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
